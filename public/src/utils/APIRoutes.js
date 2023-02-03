6b43e4535dd1ab310d9ace4b3d3520edfdfe6f8f
Jose V <valdezjose856@gmail.com>
2023-02-03 15:05:31 -0600

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "react-chat-2d2br06vl-josevaldez15.vercel.app";
+export const host = "prj_m0MpvPPJ0CCjojc2Uq2H3L91MFrY";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
