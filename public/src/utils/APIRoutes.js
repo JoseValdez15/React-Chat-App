ff7110f2df5ae8720e8d7e5c5012fa491540cdd6
Jose V <valdezjose856@gmail.com>
2023-02-02 21:12:50 -0600

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -6,5 +6,3 @@ export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
 export const recieveMessageRoute = `${host}/api/messages/getmsg`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
-
-export const baseURL = "react-chat-app-hazel.vercel.app";
